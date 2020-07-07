a10c6fa059b2d254f79962b4bb3bb814fd7980c9
Плосковитов Илья Антонович <iploskovitov@phoenixit.ru>
2020-07-07 11:06:40 +0300

    update guava version in pom

--- TestNG/src/test/java/SampleTestNG.java
+++ TestNG/src/test/java/SampleTestNG.java
@@ -1,10 +1,12 @@
 import config.ConfigLoader;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 import org.testng.Assert;
 
